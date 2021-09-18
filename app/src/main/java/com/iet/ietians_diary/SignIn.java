a11cfa0a3465c786f165185f5088f75e18fd76ca
Akshat <akshat7509999412@gmail.com>
2021-09-18 19:31:59 +0530

    status bar corrected

--- app/src/main/java/com/iet/ietians_diary/SignIn.java
+++ app/src/main/java/com/iet/ietians_diary/SignIn.java
@@ -1,7 +1,9 @@
 package com.iet.ietians_diary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class SignIn extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        /*if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+           // getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.white));
+        }*/
     }
 }
