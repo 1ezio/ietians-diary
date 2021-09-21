3ac25bad20bf052db455f00920358f5f1e56f024
gautamjain09 <gautamjain0918@gmail.com>
2021-09-21 19:11:17 +0530

    Select Details UI

---
+++ app/src/main/java/com/iet/ietians_diary/SelectDetails.java
@@ -0,0 +1,17 @@
+package com.iet.ietians_diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_details);
+
+
+
+    }
+}
