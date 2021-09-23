a32ddcac9b7e5b94237a8559a058736faf652056
Arinjay Saraf <arinjaysaraf@gmail.com>
2021-09-23 23:31:42 +0530

    initial

---
+++ app/src/main/java/com/iet/ietians_diary/Models/RoadmapModels.java
@@ -0,0 +1,17 @@
+package com.iet.ietians_diary.Models;
+
+public class RoadmapModels {
+    private String name;
+
+    public RoadmapModels(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
