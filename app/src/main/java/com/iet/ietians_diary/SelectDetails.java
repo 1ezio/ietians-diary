c7f167d7d2c54bed62b7f95cef6776ccb63b4893
Naman Mehra <naman.mehra,3000@gmail.com>
2021-10-01 22:28:58 +0530

    Connecting Database in SelectDetails

--- app/src/main/java/com/iet/ietians_diary/SelectDetails.java
+++ app/src/main/java/com/iet/ietians_diary/SelectDetails.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SelectDetails extends AppCompatActivity {
 
     Button btnNext;
-
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,12 @@ public class SelectDetails extends AppCompatActivity {
 
         btnNext = findViewById(R.id.btnNextFromSelectBranch);
         btnNext.setEnabled(false);
+
+        //DATABASE CONNECTION
+        database = FirebaseDatabase.getInstance().getReference().getDatabase();
+
+
+
 
 
 
