005cb3a90644e515a35ef7259951000a83848ba9
darvesh23 <chhatanidarvesh8@gmail.com>
2021-10-16 00:52:23 +0530

    Internship RecyclerView

--- app/src/main/java/com/iet/ietians_diary/Internship.java
+++ app/src/main/java/com/iet/ietians_diary/Internship.java
@@ -1,14 +1,43 @@
 package com.iet.ietians_diary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.iet.ietians_diary.Adapters.InternshipAdapter;
+import com.iet.ietians_diary.Models.InternshipModel;
+
+import java.util.List;
+
 public class Internship extends AppCompatActivity {
+
+    private RecyclerView internship_recyler;
+    private List<InternshipModel> internshipList;
+    private InternshipAdapter internshipAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_internship);
+
+
+        InternshipModel internship = new InternshipModel();
+        internship.setid("0");
+        internship.setcompanyName("Company_Name");
+        internship.setdescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!");
+        internship.setinternRole("role");
+        internship.setinternshipImage("Image Url Here");
+        internship.setinternshipUrl("www.google.com");
+        internshipList.add(internship);
+
+        internshipAdapter = new InternshipAdapter(this, internshipList);
+        internship_recyler.setAdapter(internshipAdapter);
+
+
+
+
     }
+
+
 }
